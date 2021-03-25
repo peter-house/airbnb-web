d5058e7536f1b0d8fecff72582d1989227ba4389
김은총 <eunchong.k153@gmail.com>
2021-03-25 13:11:01 +0900

    Add banner text to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,6 +6,7 @@ const Banner = () => {
     <>
       <div className="banner-bg">
         <img className="banner-img" srcSet="https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg?im_w=2560 1x, https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg?im_w=2560 2x"></img>
+        <div className="banner-text">호스트 분들이 있기에 가능합니다</div>
       </div>
     </>
   );
