7023672dba8e1942ebff37c0f5408a7e8ef25d61
김은총 <eunchong.k153@gmail.com>
2021-03-25 13:10:29 +0900

    Add banner image to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import css from "./Banner.css";
 
 const Banner = () => {
   return (
-  <div>
-    banenr!ㅣㅣㅣㅣㅣ aslkdjflkasjdflkasjdlfkjlk
-  </div>
+    <>
+      <div className="banner-bg">
+        <img className="banner-img" srcSet="https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg?im_w=2560 1x, https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg?im_w=2560 2x"></img>
+      </div>
+    </>
   );
 };
-
 
 export default Banner;
